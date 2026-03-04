39dcd73fa924fe021ff9a2113a59b169f0591fe6
pfyy <112098515+pfyy@users.noreply.github.com>
2026-03-04 22:25:17 +0800

    setup termux

---
+++ setup-termux.sh
@@ -0,0 +1,18 @@
+pkg upgrade
+
+pkg install git
+pkg install python python-pip
+pkg install aria2
+pkg install postgresql
+
+export ANDROID_API_LEVEL=24
+
+pip install pipx
+pipx install poetry
+pipx ensurepath
+
+poetry config installer.max-workers 1
+
+poetry install || true
+poetry run pip install psycopg[c]
+poetry install --only-root
