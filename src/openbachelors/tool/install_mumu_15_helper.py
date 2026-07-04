6c025fdbe221a02374894ec2b95572b8bb85956c
pfyy <112098515+pfyy@users.noreply.github.com>
2026-07-04 16:17:26 +0800

    no start

--- src/openbachelors/tool/install_mumu_15_helper.py
+++ src/openbachelors/tool/install_mumu_15_helper.py
@@ -10,11 +10,11 @@ def main():
 
     for exe_filepath in exe_filepath_lst:
         subprocess.run(
-            f'"{exe_filepath}" /txn_id=00000000-0000-0000-0000-000000000000 /from_orchestrator=1 /action=install_prepare',
+            f'"{exe_filepath}" /txn_id=00000000-0000-0000-0000-000000000000 /from_orchestrator=1 /action=install_prepare /auto_start=false',
             shell=True,
         )
         subprocess.run(
-            f'"{exe_filepath}" /txn_id=00000000-0000-0000-0000-000000000000 /from_orchestrator=1 /action=commit',
+            f'"{exe_filepath}" /txn_id=00000000-0000-0000-0000-000000000000 /from_orchestrator=1 /action=commit /auto_start=false',
             shell=True,
         )
 
