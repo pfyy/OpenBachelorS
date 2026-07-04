2a2a08a6b50cdbe95d14155e30a55d244d4bacfd
pfyy <112098515+pfyy@users.noreply.github.com>
2026-07-04 16:10:17 +0800

    helper

---
+++ src/openbachelors/tool/install_mumu_15_helper.py
@@ -0,0 +1,23 @@
+import subprocess
+
+from tkinter.filedialog import askopenfilenames
+
+
+def main():
+    exe_filepath_lst = askopenfilenames(
+        filetypes=[("MUMU 15 Setup", ".exe")],
+    )
+
+    for exe_filepath in exe_filepath_lst:
+        subprocess.run(
+            f'"{exe_filepath}" /txn_id=00000000-0000-0000-0000-000000000000 /from_orchestrator=1 /action=install_prepare',
+            shell=True,
+        )
+        subprocess.run(
+            f'"{exe_filepath}" /txn_id=00000000-0000-0000-0000-000000000000 /from_orchestrator=1 /action=commit',
+            shell=True,
+        )
+
+
+if __name__ == "__main__":
+    main()
