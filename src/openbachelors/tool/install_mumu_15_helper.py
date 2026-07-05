023072d6b297d9999c1278eae4c66bcae3e333e1
pfyy <112098515+pfyy@users.noreply.github.com>
2026-07-05 16:20:07 +0800

    make it last

--- src/openbachelors/tool/install_mumu_15_helper.py
+++ src/openbachelors/tool/install_mumu_15_helper.py
@@ -8,6 +8,9 @@ def main():
         filetypes=[("MUMU 15 Setup", ".exe")],
     )
 
+    exe_filepath_lst = list(exe_filepath_lst)
+    exe_filepath_lst.sort(key=lambda exe_filepath: exe_filepath.find("-nxmain-") != -1)
+
     for exe_filepath in exe_filepath_lst:
         subprocess.run(
             f'"{exe_filepath}" /txn_id=00000000-0000-0000-0000-000000000000 /from_orchestrator=1 /action=install_prepare /auto_start=false /product_version=6.2.3.0 /D=C:\\Program Files\\Netease\\MuMuPlayer',
