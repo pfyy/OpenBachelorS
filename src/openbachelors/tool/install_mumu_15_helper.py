adeceffc2b9e11e91c1c9c7989864b1c58d90263
pfyy <112098515+pfyy@users.noreply.github.com>
2026-07-05 15:29:05 +0800

    they don't use crt to parse cmd string

--- src/openbachelors/tool/install_mumu_15_helper.py
+++ src/openbachelors/tool/install_mumu_15_helper.py
@@ -10,11 +10,11 @@ def main():
 
     for exe_filepath in exe_filepath_lst:
         subprocess.run(
-            f'"{exe_filepath}" /txn_id=00000000-0000-0000-0000-000000000000 /from_orchestrator=1 /action=install_prepare /auto_start=false',
+            f'"{exe_filepath}" /txn_id=00000000-0000-0000-0000-000000000000 /from_orchestrator=1 /action=install_prepare /auto_start=false /D=C:\\Program Files\\Netease\\MuMuPlayer',
             shell=True,
         )
         subprocess.run(
-            f'"{exe_filepath}" /txn_id=00000000-0000-0000-0000-000000000000 /from_orchestrator=1 /action=commit /auto_start=false',
+            f'"{exe_filepath}" /txn_id=00000000-0000-0000-0000-000000000000 /from_orchestrator=1 /action=commit /auto_start=false /D=C:\\Program Files\\Netease\\MuMuPlayer',
             shell=True,
         )
 
