5c13fc0d686e14e121cd7c2f7570bd780fb163d2
pfyy <112098515+pfyy@users.noreply.github.com>
2026-07-05 16:05:11 +0800

    add ver flag

--- src/openbachelors/tool/install_mumu_15_helper.py
+++ src/openbachelors/tool/install_mumu_15_helper.py
@@ -10,11 +10,11 @@ def main():
 
     for exe_filepath in exe_filepath_lst:
         subprocess.run(
-            f'"{exe_filepath}" /txn_id=00000000-0000-0000-0000-000000000000 /from_orchestrator=1 /action=install_prepare /auto_start=false /D=C:\\Program Files\\Netease\\MuMuPlayer',
+            f'"{exe_filepath}" /txn_id=00000000-0000-0000-0000-000000000000 /from_orchestrator=1 /action=install_prepare /auto_start=false /product_version=6.2.3.0 /D=C:\\Program Files\\Netease\\MuMuPlayer',
             shell=True,
         )
         subprocess.run(
-            f'"{exe_filepath}" /txn_id=00000000-0000-0000-0000-000000000000 /from_orchestrator=1 /action=commit /auto_start=false /D=C:\\Program Files\\Netease\\MuMuPlayer',
+            f'"{exe_filepath}" /txn_id=00000000-0000-0000-0000-000000000000 /from_orchestrator=1 /action=commit /auto_start=false /product_version=6.2.3.0 /D=C:\\Program Files\\Netease\\MuMuPlayer',
             shell=True,
         )
 
